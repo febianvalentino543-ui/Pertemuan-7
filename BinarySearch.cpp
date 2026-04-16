d545468eb4c8685003dadce776d46b4ea056482b
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-04-16 11:35:25 +0700

    menambahkan prosedur binary search untuk mencari elemen dalam array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -66,3 +66,18 @@ void display() {
     cout << endl;
 }
 
+void binarySearch() {
+    char ulang;
+    do
+    {
+        cout << "\n====================================\n";
+        cout << "         Pencarian Binary Search      \n";
+        cout << "====================================\n";
+
+        cout << "\nMasukkan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        
